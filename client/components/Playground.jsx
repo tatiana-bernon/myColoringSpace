1097c8fcd9219372eeeed008e01989427a9cde7c
taty500summers <tatiana.bernon@gmail.com>
2020-10-07 12:01:12 +1300

    setup playground.jsx

---
+++ client/components/Playground.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, useRef, useCallback, useMemo } from 'react'
+import randomColor from  'randomcolor'
+
+export default function Playground(){
+  return (
+    <>
+      <div className="playground">
+        Welcome to your coloring space!
+      </div>
+    </>
+  )
+}
