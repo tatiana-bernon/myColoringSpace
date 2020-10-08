8da0a92af39dceae97b27b17d87cb1ae794b2004
taty500summers <tatiana.bernon@gmail.com>
2020-10-08 13:37:14 +1300

    Create state to setColor

--- client/components/Playground.jsx
+++ client/components/Playground.jsx
@@ -1,10 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import randomColor from  'randomcolor'
 
 export default function Playground() {
   const [count, setCount] = useState(0)
+  
+  const [color, setColor] = useState(null)
+  useEffect(() => {
+    setColor(randomColor())
+  }, [count])
+
   return (
-  <div>
+  <div style={{ borderTop: `10px solid ${color}`}}>
     {count}
   <button onclick={() => setCount(currentCount => currentCount -1)}> - </button>
   <button onclick={() => setCount(currentCount => currentCount + 1)}> + </button>
