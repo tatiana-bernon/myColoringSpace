afa22f4b723724e4ad84c5d4dfa7c57a8f355aac
taty500summers <tatiana.bernon@gmail.com>
2020-10-08 13:22:59 +1300

    Create onclick on button

--- client/components/Playground.jsx
+++ client/components/Playground.jsx
@@ -4,6 +4,10 @@ import randomColor from  'randomcolor'
 export default function Playground() {
   const [count, setCount] = useState(0)
   return (
-  <div>{count}</div>
+  <div>
+    {count}
+  <button onclick={() => setCount(currentCount => currentCount -1)}> - </button>
+  <button onclick={() => setCount(currentCount => currentCount + 1)}> + </button>
+  </div>
   )
 }
