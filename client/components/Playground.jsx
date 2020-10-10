54fc107a696c42c501a23fcd7c1ad325cab0179e
taty500summers <tatiana.bernon@gmail.com>
2020-10-11 07:18:06 +1300

    Refactors function calculate

--- client/components/Playground.jsx
+++ client/components/Playground.jsx
@@ -24,12 +24,6 @@ export default function Playground() {
 }
 
 function Calculate(num) {
-  // first call, num === 3... ok I will calculate that
-  return fetchComplicatedAlgorithmToAdd47(3) // returns 50 after awhile
-  
-  // second call, num === 5... ok I guess I have to calculate that too
-  return fetchComplicatedAlgorithmToAdd47(5) // returns 52 after awhile
-  
-  // third call, num === 3... WAIT, I've seen this before! I know this one!
-  return 50 // immediately
+  const renderCount = useRef(1)
+  return <div>{renderCount.current++}</div>
 }
